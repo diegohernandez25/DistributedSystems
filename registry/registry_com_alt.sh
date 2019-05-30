706341466d0e5def100bc243d8421ae0a58886c9
Diego Hernandez <diego.hernandez.c25@gmail.com>
2019-05-30 18:58:20 +0100

    Register and OutsideWorld working local. Binding works

---
+++ registry/registry_com_alt.sh
@@ -0,0 +1,5 @@
+
+java -Djava.rmi.server.codebase="file:///home/diego/Workspace/ECT/4ano/SD/pratica/sd-assingment3/registry"\
+     -Djava.rmi.server.useCodebaseOnly=false\
+     -Djava.security.policy=java.policy\
+     Main.ServerRegisterRemoteObject
